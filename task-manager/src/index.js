4bf8e4ca349b245a4f4cce4a76b6da5fecf09435
Achintya0502 <achintyapatel55555@gmail.com>
2020-02-07 01:34:54 +0530

    added email sending feature

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -5,12 +5,12 @@ const taskRouter = require('./routers/task')
 
 const app = express()
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT
 
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
 
 app.listen(port, () => {
-    console.log('Server is up on port' + port)
+    console.log('Server is up on port ' + port)
 })
